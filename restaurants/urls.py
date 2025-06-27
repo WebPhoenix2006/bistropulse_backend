2553712c99d9c95b4933cfafe728e04af47ae2c3
Samuel Emeka <devsamuel0611@gmail.com>
2025-06-27 18:35:22 +0100

    added restaurant model

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import RestaurantViewSet
 
 router = DefaultRouter()
-router.register('restaurants', RestaurantViewSet)
+router.register(r'', RestaurantViewSet, basename='restaurant')  # 👈 changed here
 
 urlpatterns = [
     path('', include(router.urls)),
