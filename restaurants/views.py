136e5a5b8aeca79d10420d00c9a6ce5c430ddd69
Samuel Emeka <devsamuel0611@gmail.com>
2025-06-28 20:02:14 +0100

    updated model to accept files uploads

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Restaurant
 from .serializers import RestaurantSerializer
 from rest_framework.permissions import IsAuthenticated
@@ -14,3 +14,9 @@ class RestaurantListCreateView(ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+class RestaurantDetailView(RetrieveUpdateDestroyAPIView):
+    serializer_class = RestaurantSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Restaurant.objects.filter(user=self.request.user)
