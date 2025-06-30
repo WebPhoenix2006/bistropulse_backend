4941147a89b8a396c66e4e14c1d7adb683859d50
Samuel Emeka <devsamuel0611@gmail.com>
2025-06-30 14:53:57 +0100

    fixing image url issue

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -3,5 +3,4 @@ from .views import RestaurantListCreateView, RestaurantDetailView
 
 urlpatterns = [
     path('', RestaurantListCreateView.as_view(), name='restaurant-list'),
-    path('<int:pk>/', RestaurantDetailView.as_view(), name='restaurant-detail'),
 ]
