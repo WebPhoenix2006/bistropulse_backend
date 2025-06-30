f8285873d6cb42f24e6a522847d56d9e27635263
Samuel Emeka <devsamuel0611@gmail.com>
2025-06-30 12:19:06 +0100

    updated model to add creation date to api response

--- customers/models.py
+++ customers/models.py
@@ -1,15 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+import random
+import string
+
+def generate_customer_id():
+    prefix = 'user#'
+    suffix = ''.join(random.choices(string.digits, k=6))
+    return f'{prefix}{suffix}'
+
 class Customer(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='customers')
-    name = models.CharField(max_length=255)
+    customer_id = models.CharField(max_length=10, unique=True, blank=True)
+    name = models.CharField(max_length=100)
     email = models.EmailField()
     phone = models.CharField(max_length=20)
     is_student = models.BooleanField(default=False)
-    gender = models.CharField(max_length=10, choices=[('Male', 'Male'), ('Female', 'Female')])
+    gender = models.CharField(max_length=10)
     location = models.TextField()
-    photo = models.ImageField(upload_to='customer_photos/', null=True, blank=True)
-    
+    photo = models.ImageField(upload_to='customer_photos/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.customer_id:
+            # Ensure uniqueness
+            while True:
+                new_id = generate_customer_id()
+                if not Customer.objects.filter(customer_id=new_id).exists():
+                    self.customer_id = new_id
+                    break
+        super().save(*args, **kwargs)
+
     def __str__(self):
-        return self.name
+        return f"{self.customer_id} - {self.name}"
--- customers/serializers.py
+++ customers/serializers.py
@@ -2,12 +2,17 @@ from rest_framework import serializers
 from .models import Customer
 
 class CustomerSerializer(serializers.ModelSerializer):
+    photo = serializers.SerializerMethodField()
+
     class Meta:
         model = Customer
+        fields = [
+            'id', 'customer_id', 'name', 'email', 'phone',
+            'is_student', 'gender', 'location', 'photo', 'created_at'
+        ]
         exclude = ['user']
-        
+
     def get_photo(self, obj):
         request = self.context.get('request')
-        if obj.photo and request:
-            return request.build_absolute_uri(obj.photo.url)
-        return None
+        return request.build_absolute_uri(obj.photo.url) if obj.photo and request else None
+
