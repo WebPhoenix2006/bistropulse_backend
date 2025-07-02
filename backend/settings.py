b2ef0c79a7463aa8203087b0402c3d24a14b9fa4
Samuel Emeka <devsamuel0611@gmail.com>
2025-07-02 19:51:43 +0100

    reverted settings.py to original

--- backend/settings.py
+++ backend/settings.py
@@ -105,3 +105,16 @@ CORS_ALLOWED_ORIGINS = [
     # Add your production frontend URL when ready
 ]
 CORS_ALLOW_CREDENTIALS = True
+CORS_EXPOSE_HEADERS = ['Content-Type', 'X-CSRFToken']
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
