048d1a22b27ddbe61e33c12893cc6e2a7c6191f2
Samuel Emeka <devsamuel0611@gmail.com>
2025-07-02 20:35:21 +0100

    reverted settings.py to original

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 from django.views.generic import TemplateView  # For health check
 
 urlpatterns = [
@@ -12,5 +12,12 @@ urlpatterns = [
 
     # Health check endpoint for Render
     path('api/health/', TemplateView.as_view(template_name='health_check.html'), name='health-check'),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+    # Media serving in production (Render)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
+
+# Only serve static files using Django in development
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
