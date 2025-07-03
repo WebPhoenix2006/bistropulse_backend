06fc7c781562f8b8c7b40096f0378cfd4240097a
Samuel Emeka <devsamuel0611@gmail.com>
2025-07-03 10:45:51 +0100

    updated customers view

--- customers/urls.py
+++ customers/urls.py
@@ -3,9 +3,5 @@ from .views import CustomerDetailView, CustomerListCreateView
 
 urlpatterns = [
     path("", CustomerListCreateView.as_view(), name="customers"),
-    path(
-        "customers/<str:customer_id>/",
-        CustomerDetailView.as_view(),
-        name="customer-detail",
-    ),
+    path("customers/<str:customer_id>/", CustomerDetailView.as_view()),
 ]
