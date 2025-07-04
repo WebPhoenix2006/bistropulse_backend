26be9aee746d85cd0b7fd5e8489702a596fb60b5
Samuel Emeka <devsamuel0611@gmail.com>
2025-07-04 12:20:51 +0100

    fixing put error

--- customers/urls.py
+++ customers/urls.py
@@ -3,5 +3,5 @@ from .views import CustomerDetailView, CustomerListCreateView
 
 urlpatterns = [
     path("", CustomerListCreateView.as_view(), name="customers"),
-    path("customers/<str:customer_id>/", CustomerDetailView.as_view()),
+    path("<str:customer_id>/", CustomerDetailView.as_view(), name="customer-detail"),
 ]
