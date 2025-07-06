0fa28fe9db6ce32851aaaff5413c60d14842f73e
Samuel Emeka <devsamuel0611@gmail.com>
2025-07-06 23:24:07 +0100

    updated restaurants model

--- restaurants/serializers.py
+++ restaurants/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
-from .models import Restaurant, Food, Extra, FoodCategory
+from .models import Restaurant, FoodCategory, Food, Review
+from .serializers import (
+    FoodCategorySerializer,
+    FoodSerializer,
+    ReviewSerializer,
+)  # Assume these exist
 
 
 class RestaurantSerializer(serializers.ModelSerializer):
-    restaurant_image_url = serializers.SerializerMethodField(read_only=True)
+    restaurant_image_url = serializers.SerializerMethodField()
+    categories = serializers.SerializerMethodField()
+    foods = serializers.SerializerMethodField()
+    reviews = serializers.SerializerMethodField()
 
     class Meta:
         model = Restaurant
@@ -21,8 +29,10 @@ class RestaurantSerializer(serializers.ModelSerializer):
             "restaurant_image_url",
             "rating",
             "status",
+            "categories",
+            "foods",
+            "reviews",
         ]
-        read_only_fields = ["restaurant_image_url"]
 
     def get_restaurant_image_url(self, obj):
         request = self.context.get("request")
@@ -30,63 +40,18 @@ class RestaurantSerializer(serializers.ModelSerializer):
             return request.build_absolute_uri(obj.restaurant_image.url)
         return None
 
-    def create(self, validated_data):
-        user = self.context["request"].user
-        validated_data.pop("user", None)
-        return Restaurant.objects.create(user=user, **validated_data)
+    def get_categories(self, obj):
+        categories = obj.categories.all()
+        return (
+            FoodCategorySerializer(categories, many=True).data
+            if categories.exists()
+            else []
+        )
 
+    def get_foods(self, obj):
+        foods = obj.foods.all()
+        return FoodSerializer(foods, many=True).data if foods.exists() else []
 
-class ExtraSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Extra
-        fields = ["id", "name", "price"]
-
-
-class FoodCategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = FoodCategory
-        fields = ["id", "name"]
-
-
-class FoodSerializer(serializers.ModelSerializer):
-    extras = ExtraSerializer(many=True, read_only=True)
-    category = FoodCategorySerializer(read_only=True)
-    category_id = serializers.PrimaryKeyRelatedField(
-        queryset=FoodCategory.objects.all(), source="category", write_only=True
-    )
-
-    class Meta:
-        model = Food
-        fields = [
-            "id",
-            "name",
-            "description",
-            "price",
-            "image",
-            "category",
-            "category_id",
-            "extras",
-            "restaurant",
-        ]
-
-
-def get_categories(self, obj):
-    return (
-        FoodCategorySerializer(obj.categories.all(), many=True).data
-        if obj.categories.exists()
-        else []
-    )
-
-
-def get_foods(self, obj):
-    return (
-        FoodCategory(obj.categories.all(), many=True).data
-        if obj.categories.exists()
-        else []
-    )
-
-
-def get_extras(self, obj):
-    return (
-        Extra(obj.categories.all(), many=True).data if obj.categories.exists() else []
-    )
+    def get_reviews(self, obj):
+        reviews = obj.reviews.all()
+        return ReviewSerializer(reviews, many=True).data if reviews.exists() else []
