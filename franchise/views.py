14469873cfe9e7ebdc61cecb785cb975c06f267a
Samuel Emeka <devsamuel0611@gmail.com>
2025-07-27 20:26:24 +0100

    updated branch model to accept files

--- franchise/views.py
+++ franchise/views.py
@@ -1,5 +1,7 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.parsers import MultiPartParser, FormParser
+
 from .models import Franchise, Branch
 from .serializers import FranchiseSerializer, BranchSerializer
 
@@ -8,11 +10,13 @@ class FranchiseListCreateView(generics.ListCreateAPIView):
     queryset = Franchise.objects.all()
     serializer_class = FranchiseSerializer
     permission_classes = [IsAuthenticated]
+    parser_classes = [MultiPartParser, FormParser]  # 👈 Added for file uploads
 
 
 class BranchListCreateView(generics.ListCreateAPIView):
     serializer_class = BranchSerializer
     permission_classes = [IsAuthenticated]
+    parser_classes = [MultiPartParser, FormParser]  # 👈 Added for file uploads
 
     def get_queryset(self):
         franchise_id = self.kwargs["franchise_id"]
