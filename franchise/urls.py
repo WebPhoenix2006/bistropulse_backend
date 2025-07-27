104f3fbd0b32c6ae117c9b80ee53ecf3e66b5c4f
Samuel Emeka <devsamuel0611@gmail.com>
2025-07-27 20:32:07 +0100

    updated branch model to accept files

--- franchise/urls.py
+++ franchise/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from .views import FranchiseListCreateView, BranchListCreateView
 
 urlpatterns = [
-    path('', FranchiseListCreateView.as_view(), name='franchise-list-create'),
-    path('<int:franchise_id>/branches/', BranchListCreateView.as_view(), name='branch-list-create'),
+    path("", FranchiseListCreateView.as_view(), name="franchise-list-create"),
+    path(
+        "<str:franchise_id>/branches/",
+        BranchListCreateView.as_view(),
+        name="branch-list-create",
+    ),
 ]
