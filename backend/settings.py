bbc4f40206ae1b765e94c523adb90a8cef65a5d7
Samuel Emeka <devsamuel0611@gmail.com>
2025-08-05 13:49:33 +0100

    fixed drf-spectacular

--- backend/settings.py
+++ backend/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     "corsheaders",
     "channels",
-    "drf-spectacular",
+    "drf_spectacular",
     # Local apps
     "authapp",
     "restaurants",
