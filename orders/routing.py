9b167887bd3d20338b0629641f3fe81f3b3792a3
Samuel Emeka <devsamuel0611@gmail.com>
2025-08-13 14:02:27 +0100

    serving with daphne

--- orders/routing.py
+++ orders/routing.py
@@ -4,6 +4,11 @@ from . import consumers  # OrderTrackingConsumer
 from chat import consumers as chat_consumers  # Your TestConsumer
 
 websocket_urlpatterns = [
-    re_path(r"ws/orders/(?P<order_id>\d+)/$", consumers.OrderTrackingConsumer.as_asgi()),
-    re_path(r"ws/restaurant/(?P<restaurant_id>\d+)/$", consumers.RestaurantOrdersConsumer.as_asgi()),
+    re_path(
+        r"ws/orders/(?P<order_id>[\w\d]+)/$", consumers.OrderTrackingConsumer.as_asgi()
+    ),
+    re_path(
+        r"ws/restaurant/(?P<restaurant_id>\d+)/$",
+        consumers.RestaurantOrdersConsumer.as_asgi(),
+    ),
 ]
